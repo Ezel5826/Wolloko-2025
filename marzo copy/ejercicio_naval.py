aefd32d4ab75321de32859907c917a4fea7692f0
Ezel5826 <ligockiezequiel@gmail.com>
2025-04-15 15:25:06 -0300

    Batalla_naval2

--- marzo copy/ejercicio_naval.py
+++ marzo copy/ejercicio_naval.py
@@ -5,8 +5,9 @@ ancho=10
 alto=10
 cantidad_barcos=[1,2,2,3,3,4]
 
-
-def grilla_juego(alto,ancho):
+def grilla_juego(alto:int,ancho:int)-> list:
+    """crear y devuelve la grilla del juego"""
+    
     juego=[]
     for i in range(ancho):
         lista_aux=[]
@@ -15,17 +16,17 @@ def grilla_juego(alto,ancho):
         juego.append(lista_aux)
     return juego
 
-def insertar_barco(barco,grilla:list):
+def insertar_barco(barco:list[list[int]],grilla:list) -> list[list[int]]:
+    """inserta el barco en la grilla y devuelve la grilla modificada"""
     for i in range(len(barco)):
         grilla[barco[i][0]][barco[i][1]]=1
     return grilla
 
-def crear_barcos_hor(long:list,cord_piv,grilla,lista):
+def crear_barcos_hor(long:list,cord_piv:tuple[int],grilla:list,lista:list)-> list[list[int]]:
+    """crea barcos de manera horizontal y añade sus cordenadas en una lista"""
     barco=[cord_piv]
-    print(barco)
     for i in range(1,long):
         barco.append([cord_piv[0],cord_piv[1]+i])
-    print(deteccion_limite(barco,grilla))
     if deteccion_limite(barco,grilla):
         lista=almacenar_barco(barco,lista,grilla)
         return barco,lista
@@ -33,12 +34,11 @@ def crear_barcos_hor(long:list,cord_piv,grilla,lista):
         barco,lista=reeleccion_barco(lista,grilla,long,1)
         return barco,lista
     
-def crear_barcos_vert(long,cord_piv,grilla,lista):
+def crear_barcos_vert(long:int,cord_piv:tuple[int],grilla:list,lista:list)-> list[list[int]] | list[int]:
+    """crea barcos de manera horizontal y añade sus cordenadas en una lista"""
     barco=[cord_piv]
-    print(barco)
     for i in range(1,long):
         barco.append([cord_piv[0]+i,cord_piv[1]])
-    print(deteccion_limite(barco,grilla))
     if deteccion_limite(barco,grilla):
         lista=almacenar_barco(barco,lista,grilla)
         return barco,lista
@@ -46,7 +46,8 @@ def crear_barcos_vert(long,cord_piv,grilla,lista):
         barco,lista=reeleccion_barco(lista,grilla,long)
         return barco,lista
     
-def barco_con_sentido(long,cord_piv,grilla,lista, sent):
+def barco_con_sentido(long:int,cord_piv:tuple[int],grilla:list,lista:list, sent:int)-> list[list[int]] | list[int]:
+    """elige la forma de crear el barco, horizontal o vertical y devuelve el barco creado"""
     long1=long[0]
     long.pop(0)
     if sent:
@@ -54,11 +55,11 @@ def barco_con_sentido(long,cord_piv,grilla,lista, sent):
     else:
         return crear_barcos_vert(long1,cord_piv,grilla,lista)
 
-def reeleccion_barco(lista_barcos,grilla,long,H_sen=0):
+def reeleccion_barco(lista_barcos:list[int],grilla:list,long:int,H_sen:int=0)-> list[list[int]] | list[int]:
+    """reelige la forma de crear el barco, horizontal o vertical y devuelve el barco creado"""
     j=0
     while True:
         if j > 100:
-            print("se cumple?")
             return None,lista_barcos
         barco=[[randint(0,ancho-1),randint(0,alto-1)]]
         x,y=coordenadas_barco(barco)
@@ -72,24 +73,15 @@ def reeleccion_barco(lista_barcos,grilla,long,H_sen=0):
             return barco,lista_barcos
         j+=1
             
-def not_barco_in_lista(barco,lista):
+def not_barco_in_lista(barco:list[list[list]],lista:list)->bool:
+    """verifica si el barco no se encuentra en la lista de barcos"""
     for i in range(len(barco)):
         if barco[i] in lista:
             return False
     return True
 
-def deteccion_limite(barco,grilla):
-    contador=0
-    for i in range(len(barco)):
-        x,y=coordenadas_barco([barco[i]])
-        if no_pasa_limite(x,y,grilla) and grilla_vacia(x,y,grilla):
-            contador+=1
-    if contador == len(barco):
-        return True
-    else:
-        return False
-
-def almacenar_barco(barco:tuple,lista:list,grilla:list):
+def almacenar_barco(barco:tuple,lista:list,grilla:list)->list:
+    """almacena el barco en la lista de barcos"""
     x,y=coordenadas_barco(barco)
     for k in range(x-1,x+2):
         for l in range(y-1,y+2):
@@ -97,7 +89,9 @@ def almacenar_barco(barco:tuple,lista:list,grilla:list):
                 lista.append((k,l))
     return lista
 
-def agregar_adyacentes(barco, grilla):
+
+def agregar_adyacentes(barco:list[list[int]], grilla:list)->list:
+    """agrega las coordenadas adyacentes a la grilla"""
     for i in range(len(barco)):
         x,y=coordenadas_barco([barco[i]])
         for k in range(x-1,x+2):
@@ -106,27 +100,32 @@ def agregar_adyacentes(barco, grilla):
                         grilla[k][l]=2
     return grilla
 
-def enviar_disparos(disparo,grilla:list):
-    x,y=coordenadas_disparo(disparo)
-    if grilla_ocupada(x,y,grilla):
-            grilla[x][y]=3
 
-    else:
-        return grilla
-
+# funciones relacionadas con el disparo
 def elegir_disparo(lista:list):
+    """elige un disparo aleatorio y verifica que no se repita"""
     while True:
         disparo=(randint(0,ancho-1), randint(0,alto-1))
         if disparo_diferente(disparo,lista):
             return disparo
 
-def disparo_diferente(disparo:tuple,lista_disparos:list):
+def enviar_disparos(disparo:tuple[int],grilla:list)->list:
+    """envia el disparo y devuelve la grilla modificada"""
+    x,y=coordenadas_disparo(disparo)
+    if grilla_ocupada(x,y,grilla):
+            grilla[x][y]=3
+    else:
+        return grilla
+
+def disparo_diferente(disparo:tuple,lista_disparos:list)->bool:
+    """verifica que el disparo no se repita"""
     for i in range(len(lista_disparos)):
         if disparo==lista_disparos[i]:
             return False
     return True
 
-def almacenar_disparos(disparo:tuple,grilla:list,lista_disparos:list):
+def almacenar_disparos(disparo:tuple,grilla:list,lista_disparos:list)->list[int]:
+    """almacena los disparos en una lista"""
     x,y=coordenadas_disparo(disparo)
     if grilla_ocupada(x,y,grilla):
         for k in range(x-1,x+2):
@@ -137,22 +136,40 @@ def almacenar_disparos(disparo:tuple,grilla:list,lista_disparos:list):
         lista_disparos.append((x,y))
     return lista_disparos
     
-def coordenadas_barco(barco):
+def deteccion_limite(barco:list[list[int]],grilla:list)->bool:
+    """verifica si el barco no se pasa del limite de la grilla y si la  grilla en las cordenadas del barco está vacia"""
+    contador=0
+    for i in range(len(barco)):
+        x,y=coordenadas_barco([barco[i]])
+        if no_pasa_limite(x,y,grilla) and grilla_vacia(x,y,grilla):
+            contador+=1
+    if contador == len(barco):
+        return True
+    else:
+        return False
+    
+def coordenadas_barco(barco:list[list[int]])->tuple[int,int]:
+    """devuelve las coordenadas del barco"""
     return barco[0][0], barco[0][1]
 
-def coordenadas_disparo(disparo):
+def coordenadas_disparo(disparo:list[list[int]])->tuple[int,int]:
+    """devuelve las coordenadas del disparo"""
     return disparo[0], disparo[1]
 
-def grilla_vacia(x:int,y:int,grilla:list):
+def grilla_vacia(x:int,y:int,grilla:list)->bool:
+    """verifica si la grilla está vacia"""
     return grilla[x][y]==0
 
-def grilla_ocupada(x:int,y:int,grilla:list):
+def grilla_ocupada(x:int,y:int,grilla:list)->bool:
+    """verifica si la grilla está ocupada"""
     return grilla[x][y]==1
 
-def no_pasa_limite(x:int,y:int,grilla:list):
+def no_pasa_limite(x:int,y:int,grilla:list)->bool:
+    """verifica si el barco no se pasa del limite de la grilla"""
     return 0 <= x <= len(grilla[0])-1 and 0 <= y <= len(grilla)-1
 
 def contador_barcos(grilla:list):
+    """cuenta la cantidad de barcos en la grilla"""
     contador=0
     for i in range(len(grilla[0])):
         for j in range(len(grilla)):
@@ -160,8 +177,8 @@ def contador_barcos(grilla:list):
                 contador+=1
     return contador
 
-
-def limite_de_barcos(barcos:tuple,cantidad:int):
+def limite_de_barcos(barcos:tuple,cantidad:int)->bool:
+    """verifica si se ha alcanzado el limite de barcos"""
     return barcos==cantidad
 
 
